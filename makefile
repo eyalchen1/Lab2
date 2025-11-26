48fd77f62a950b761acaa64f77944643ce094e23
caspl202 <cheneya@post.bgu.ac.il>
2025-11-26 11:03:56 +0200

    all done

--- makefile
+++ makefile
@@ -2,9 +2,12 @@ all: clean link
 link: compile 
 	gcc -g -m32 -Wall -o myshell LineParser.o myshell.o
 	gcc -g -m32 -Wall -o looper LineParser.o Looper.o
-compile: myshell.c LineParser.c Looper.c
+	gcc -g -m32 -Wall -o mypipe mypipe.o
+compile: myshell.c LineParser.c Looper.c mypipe.c
 	gcc -m32 -g -Wall -c -o myshell.o myshell.c
 	gcc -m32 -g -Wall -c -o LineParser.o LineParser.c
 	gcc -m32 -g -Wall -c -o Looper.o Looper.c
+	gcc -m32 -g -Wall -c -o mypipe.o mypipe.c
+
 clean:
 	rm -f *.o myshell
---
+++ mypipe.c
@@ -0,0 +1,29 @@
+#include <stdio.h>      // for printf
+#include <unistd.h>     // for pipe, fork, read, write, close
+#include <string.h>     // for strlen
+#include <sys/wait.h>   // for wait, NULL
+#include <stdlib.h>     // optional: for exit()
+
+
+int main(int argc, char *argv[]){
+    int fd[2];
+    char buffer[2048];
+    if(pipe(fd) == -1){
+        printf("error");
+        return 1;
+    }
+    int pid= fork();
+    if(!pid){
+        close(fd[0]);
+        write(fd[1], argv[1], strlen(argv[1])+1);
+        close(fd[1]);
+    }
+    else{
+        wait(NULL);
+        close(fd[1]);
+        read(fd[0], buffer, sizeof(buffer));
+        printf("%s\n", buffer);
+        close(fd[0]);
+    }
+
+}
